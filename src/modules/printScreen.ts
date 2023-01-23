c8c96977c5790cc4b4426edb62078dab49bffa83
Raman Liausha <Raman_Liausha@epam.com>
2023-01-23 23:48:11 +0300

    fix: change logic in print screen

--- src/modules/printScreen.ts
+++ src/modules/printScreen.ts
@@ -1,24 +1,16 @@
 import Jimp from 'jimp/es';
-import { mouse, Region, screen, FileType } from '@nut-tree/nut-js';
+import { mouse, Region, screen } from '@nut-tree/nut-js';
 import { Duplex } from 'stream';
 
 export async function makePrintScreen(duplex: Duplex) {
   const { x, y } = await mouse.getPosition();
 
   const imageRegion = new Region(x - 100, y - 100, 200, 200);
-  const pathImage = await generateCaptureRegion(imageRegion);
-  const imageBase64 = await convertImageToBase64(pathImage);
+  const image = await screen.grabRegion(imageRegion);
+  const imageRGB = await image.toRGB();
+  const createImage = new Jimp(imageRGB);
+  const imageBase64 = await createImage.getBase64Async(Jimp.MIME_PNG);
+
   duplex.write(`prnt_scrn ${imageBase64.split(',')[1]}`);
   console.log('make prt', imageBase64);
-}
-
-async function generateCaptureRegion(imageRegion: Region): Promise<string> {
-  const fileFormat = FileType.PNG;
-
-  return screen.captureRegion('screen', imageRegion, fileFormat);
-}
-
-async function convertImageToBase64(path: string) {
-  const image = await Jimp.read(path);
-  return image.getBase64Async(Jimp.MIME_PNG);
 }
